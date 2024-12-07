7b88b59aa9cfc4f077f3b59139454abf19db54a1
youzhiyang <1454900087@qq.com>
2024-12-07 21:20:14 +0800

    MINOR: 统一的宏开关启用mimalloc

--- OpenXLSX/sources/ExtensionForSharp.cpp
+++ OpenXLSX/sources/ExtensionForSharp.cpp
@@ -2,15 +2,20 @@
 #include <iostream>
 #include <mimalloc.h>
 #include <pugixml.hpp>
-#include <mimalloc-new-delete.h>
+#define USE_MIMALLOC true
+#if USE_MIMALLOC
+#    include <mimalloc-new-delete.h>    // 全局替换new和delete，将内存操作指向mimalloc
+#endif                                  // USE_MIMALLOC
+
 static bool has_set_pugi_mem_func = false;
 
 void init_mimalloc_to_pugi()
 {
+#if USE_MIMALLOC
     if (!has_set_pugi_mem_func) {
         pugi::set_memory_management_functions(mi_malloc, mi_free);
         has_set_pugi_mem_func = true;
         std::cout << "set_memory_management_functions" << mi_malloc << " " << mi_free << "\n";
     }
-   
+#endif    // USE_MIMALLOC
 }
